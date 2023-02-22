af3205cc5dd22034fb0ae44c6ba64e5fd69a952b
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-02-22 22:46:01 +0530

    connecting files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 
+import Header from './Components/Header';
+import "./Styles/App.scss";
+import "./Styles/Header.scss";
+
 function App() {
   return (
     <Router>
+    <Header/>
       <Routes>
         <Route   />
       </Routes>
