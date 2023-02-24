c8295b79dee12bc62f0b55d586ff4a96857ad675
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-02-24 15:04:47 +0530

    adding some css

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+       <main>
+        <h1>Contact me </h1>
+        <form>
+            <div>
+                <label> Name </label>
+                <input type="text" required placeholer ="abc"/>
+            </div>
+
+            <div>
+                <label> Email </label>
+                <input type="text" required placeholer ="abc"/>
+            </div>
+
+            <div>
+                <label> Name </label>
+                <input type="email" required placeholer ="Abc@gmail.cpm"/>
+            </div>
+
+            <div>
+                <label> Message </label>
+                <input type="text" required placeholer ="Tell us about your query..."/>
+            </div>
+
+            <button type="submit" > Send</button>
+        </form>
+       </main> 
+    </div>
   )
 }
 
