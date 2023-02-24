20b46c71d96bdc699eb70eadcc23ab64c7cf27b8
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-02-24 14:55:29 +0530

    adding contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
-import Header from './Components/Header';
-import Home from './Components/Home'
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Contact from "./Components/Contact";
 
 import "./Styles/App.scss";
 import "./Styles/Header.scss";
 import "./Styles/Home.scss";
+import "./Styles/Footer.scss";
+import "./Styles/Contact.scss";
 
 function App() {
   return (
     <Router>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact/>} />
       </Routes>
+      <Footer />
     </Router>
-  )
+  );
 }
 export default App;
- 
