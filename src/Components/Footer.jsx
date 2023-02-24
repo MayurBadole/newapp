5d4edd0daa998b4cee248da68b1b85db87b30441
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-02-24 14:29:23 +0530

    create footer and styling it

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+ 
+
+function Footer() {
+  return (
+    <Footer>
+        <div>
+            <h1>
+                Mayura
+            </h1>
+            <p> @all right reserved</p>
+        </div>
+        <div>
+        <h5> Follow me</h5>
+        <div>
+            <a href='https://www.linkedin.com/in/mayur-badole-599632213/' target={'blank'
+            }> linkedin</a>
+            
+        </div>
+        </div>
+    </Footer>
+  )
+}
+
+export default Footer
